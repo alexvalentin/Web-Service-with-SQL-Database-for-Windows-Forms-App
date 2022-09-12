d09e3d9f49f323efb7fd2ebe147926cdb7f0bd73
Alexandru-Valentin Constantin <constantin.valentin0102@gmail.com>
2022-09-12 20:33:31 +0300

    Add project files.

---
+++ WebServiceHomework2.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="WebServiceHomework2.asmx.cs" Class="Web_Service_Homework2.WebService_Homework2" %>
---
+++ WebServiceHomework2.asmx.cs
@@ -0,0 +1,56 @@
+﻿using System.Web.Services;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Web_Service_Homework2
+{
+    /// <summary>
+    /// Summary description for WebServicewithSQLDBConnection
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService_Homework2 : System.Web.Services.WebService
+    {
+        SqlConnection myConnection = new SqlConnection();
+        DataSet dataset;
+
+        [WebMethod]
+
+        public DataSet DataSetInventar()
+        {
+            myConnection.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Alexandru-Valentin C\source\repos\Web Service Homework2\App_Data\Database1.mdf;Integrated Security=True;";
+            myConnection.Open();
+
+            dataset = new DataSet();
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("SELECT * FROM Inventar", myConnection);
+
+            sqlDataAdapter.Fill(dataset, "Inventar");
+
+            myConnection.Close();
+
+            return dataset;
+        }
+
+        [WebMethod]
+        public void CommandinInventar(string command)
+        {
+
+            myConnection.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Alexandru-Valentin C\source\repos\Web Service Homework2\App_Data\Database1.mdf;Integrated Security=True;";
+            //"C:\Users\Alexandru-Valentin C\source\repos\Web Service Homework2\App_Data\Database1.mdf"
+
+            myConnection.Open();
+
+            SqlCommand sqlCommand = new SqlCommand();
+
+            sqlCommand.Connection = myConnection;
+            sqlCommand.CommandText = command;
+
+            sqlCommand.ExecuteNonQuery();
+
+        }
+    }
+}
